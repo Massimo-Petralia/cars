e91f5747612c68d0a09c5f75bf25b869f58176fc
massimo <massipetra@hotmail.it>
2019-05-22 16:04:27 +0200

    hTTPcLIENT ADDED

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Cars';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+
 }
