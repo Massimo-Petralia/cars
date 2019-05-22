b548757248794ea5d4cc2889c4e671af7ad574c9
massimo <massipetra@hotmail.it>
2019-05-22 16:43:51 +0200

    cars list work!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Icar } from './car';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,21 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   title = 'Cars';
   dbUrl = 'http://localhost:3000/cars'
+  cars: Icar[] = [];
   constructor(private http: HttpClient) {
 
   }
 
   ngOnInit() {
-
+    this.getCars()
   }
 
 
   getCars() {
-    this.http.get(this.dbUrl).subscribe()
+    this.http.get(this.dbUrl).subscribe((cars: Icar[]) => {
+      this.cars = cars;
+      console.log(this.cars)
+    })
   }
 
 }
--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Icar } from '../car';
 @Component({
   selector: 'app-cars-list',
   templateUrl: './cars-list.component.html',
   styleUrls: ['./cars-list.component.scss']
 })
 export class CarsListComponent implements OnInit {
+
+  @Input() cars: Icar[] = []; 
+
 
   constructor() { }
 
